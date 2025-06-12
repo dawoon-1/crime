bff87a00b67ee35fbed7f4a6ff95a6cd08be84a2
dawoon-1 <g5ohno5@naver.com>
2025-06-12 20:54:18 +0000

    여섯

--- check_encoding.py
+++ check_encoding.py
@@ -1,11 +1,11 @@
-import chardet
+import check_encoding
 
 filename = 'crime_data.csv'
 
 with open(filename, 'rb') as f:
     rawdata = f.read(10000)  # 앞 10,000바이트 정도만 읽음
 
-result = chardet.detect(rawdata)
+result = check_encoding.detect(rawdata)
 encoding = result['encoding']
 
 print(f"추측된 인코딩: {encoding}")
